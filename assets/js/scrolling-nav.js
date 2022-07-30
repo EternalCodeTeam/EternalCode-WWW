07adde59b853586d9db651e9815e61aa183a10e2
Martin Sulikowski <vLuckyyy.biznes@gmail.com>
2022-07-31 00:37:25 +0200

    change some things

--- assets/js/scrolling-nav.js
+++ assets/js/scrolling-nav.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function() {
     // Add smooth scrolling to all links
     $("a").on('click', function(event) {
 
@@ -8,13 +8,13 @@ $(document).ready(function(){
             event.preventDefault();
 
             // Store hash
-            var hash = this.hash;
+            const hash = this.hash;
 
             // Using jQuery's animate() method to add smooth page scroll
             // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
             $('html, body').animate({
                 scrollTop: $(hash).offset().top
-            }, 800, function(){
+            }, 800, function() {
 
                 // Add hash (#) to URL when done scrolling (default click behavior)
                 window.location.hash = hash;
