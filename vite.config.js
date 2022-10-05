efd2b27aaa77f56313e308bb2b81d28d2eea9b05
Krzysztof Haller <contact@khaller.com>
2022-10-04 22:29:15 -0700

    GH-27 Separate i18n languages files (#27)
    * :sparkles: Start moving translations from main to separated files
    
    * :sparkles: Implement separating lang files

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
+import { fileURLToPath, URL } from "node:url";
+import { resolve, dirname } from "node:path";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import VueI18nPlugin from "@intlify/unplugin-vue-i18n/vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue()]
+  plugins: [
+    vue(),
+    VueI18nPlugin({
+      include: resolve(dirname(fileURLToPath(import.meta.url)), './src/lang/**'),
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
 });
