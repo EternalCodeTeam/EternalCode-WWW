6b6baf620201790bf779d01cf6444bc4ddf6edd1
CitralFlo <wojtas.michal90@gmail.com>
2023-03-10 16:27:16 +0100

    Change logos for GitHub, Spigot, Modrinth to more minimalistic versions and adjust the design

--- src/components/projects/components/Project.vue
+++ src/components/projects/components/Project.vue
@@ -17,7 +17,7 @@
               <li v-if="githubUrl">
                 <a :href="githubUrl" target="_blank">
                   <button class="button-link">
-                    <img alt="githubUrl" src="/assets/img/projects/github-logo.webp">
+                    <img alt="githubUrl" src="/assets/img/projects/GitHub-logo-short.webp">
                   </button>
                 </a>
               </li>
@@ -26,7 +26,7 @@
               <li v-if="spigotUrl">
                 <a :href="spigotUrl" target="_blank">
                   <button class="button-link">
-                    <img alt="spigotUrl" src="/assets/img/projects/spigot-logo.webp">
+                    <img alt="spigotUrl" src="/assets/img/projects/spigot-logo-short.webp">
                   </button>
                 </a>
               </li>
@@ -35,7 +35,7 @@
               <li v-if="modrinthUrl">
                 <a :href="modrinthUrl" target="_blank">
                   <button class="button-link">
-                    <img alt="modrinthUrl" src="/assets/img/projects/modrinth-logo.webp">
+                    <img alt="modrinthUrl" src="/assets/img/projects/modrinth-logo-short.webp">
                   </button>
                 </a>
               </li>
@@ -66,32 +66,40 @@ export default {
 
 @media only screen and (max-width: 1000px) {
   .src-buttons {
-
-    display: inline-block;
-    justify-content: center;
+    width: 70%;
+    position: center;
+    display: flex;
     align-items: center;
-  }
 
+  }
+  .src-buttons li{
+    width: 100%;
+    margin-right: 5px;
+  }
+  .button-link {
+    height: 40px;
+    width: 40px;
+  }
   .src-buttons img {
-    margin-top: 10px;
+    margin-top: 5px;
   }
 }
+
 
 .src-buttons {
   display: flex;
-  /* W celu wyśrodkowania przycisków wyłącz z komentarza kod: i zmien `space-between` na center \/ */
-  justify-content: space-between;
-  /* align-items: center; */
-  /* justify-items: center; */
-  list-style-type: none;
+  list-style: none;
+  gap: 5px;
   padding: 0;
-  width: 100%;
+  width: 50%;
   margin-top: 15px;
+  height: auto;
 }
 
 .src-buttons li {
-  width: 33.33%;
+  width: 30%;
   margin-right: 10px;
+  height: 100%;
 }
 
 .src-buttons li:last-child {
@@ -101,22 +109,25 @@ export default {
 .button-link {
   border-style: solid;
   border-color: #6d6d6d;
-  border-radius: 20px;
-  border-width: 4px;
-  background: none;
-  width: 100%;
+  border-radius: 50%;
+  border-width: 3px;
+  background-color: transparent;
+  height: 50px;
+  width: 50px;
   max-width: 200px;
   display: flex;
   align-items: center;
   justify-content: center;
-  height: 50px;
+  max-height: 50px;
 }
 
 .button-link:hover {
   filter: drop-shadow(0 0 0.75rem rgb(150, 150, 150));
-  transform: translateY(-5px);
   background-color: #6d6d6d;
   border-color: #3d3d3d;
+  color: #111;
+  transition: 0.3s ease;
+
 }
 
 .button-link:hover img {
@@ -125,8 +136,10 @@ export default {
 
 .src-buttons img {
   filter: brightness(0) invert(0.50);
-  width: 100%;
-  height: auto;
+  max-width: 50px;
+  max-height: 50px;
+  height: 70%;
+  width: auto;
 }
 
 </style>
