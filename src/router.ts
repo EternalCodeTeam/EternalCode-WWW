992411f4ae552e71abc8329b9963eb6970828671
Martin Sulikowski <vluckyyy.biznes@gmail.com>
2023-04-23 17:41:09 +0200

    Update router.ts

--- src/router.ts
+++ src/router.ts
@@ -22,10 +22,12 @@ const router = createRouter({
     history: createWebHistory(),
     routes,
     scrollBehavior(to, from, savedPosition) {
+        if (savedPosition) {
+            return savedPosition;
+        }
+
         if (to.hash) {
-            return {
-                el: to.hash,
-            }
+            return { el: to.hash }
         }
     }
 });
